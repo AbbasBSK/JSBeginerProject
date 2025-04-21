0a3f01363430c3ca04bd577572c0953c02b3f688
baqaB4731 <baqa4731@gmail.com>
2025-04-21 20:05:41 +0330

    after DOM topic video 41

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// let text1 =  document.getElementById("mytext")
+
+// console.log(text1.innerText);
+
+
+// text1.innerText = "test"
+
+let text2= document.getElementsByClassName("mytext")
+
+console.log(text2[1].innerText);
+
