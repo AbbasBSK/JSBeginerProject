abf637738a6bb408fb1dae1e471c632e78c464dc
baqaB4731 <baqa4731@gmail.com>
2025-05-06 23:24:27 +0330

    after practise of loop and before design by HTML , CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
-for (let i = 1; i <= 10; i++) {
+const magicNumber = 7;
 
-  if (i === 5) break
-  console.log(i);
+for (attempt = 1 ; attempt <=5 ; attempt++){
+  let guess = Number(prompt("عدد جادویی بین 1 تا 10 رو حدس بزن:"));
+
+  if (guess < 0) {
+    alert("عدد منفی مجاز نیست!");
+    continue
+  }
+
+  if (guess === magicNumber) {
+    alert("آفرین! درست حدس زدی 🎉");
+    break
+  }else{
+    alert("اشتباهه! دوباره امتحان کن...");
+  }
+
+  if (attempt === 5) {
+    alert("تموم شد! عدد جادویی " + magicNumber + " بود.");
+  }
 }
