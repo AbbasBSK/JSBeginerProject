f7e893d2155b524146ea33ce0b32e6eb63f06b0a
baqaB4731 <baqa4731@gmail.com>
2025-05-08 22:31:02 +0330

    after introduction string metods video54

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,10 @@
-const magicNumber = Math.ceil( Math.random()*10);
+const text1 = "abbas";
+const text2 = "my name is abbas";
 
-let attempt = 1
-while ( attempt <= 5) {
-  attempt++
-  let guess = Number(prompt("عدد جادویی بین 1 تا 10 رو حدس بزن:"));
+console.log(text1.repeat(3));
 
-  if (guess === 0) {
-    alert("شما منصرف شدید");
-    break
-  }
-  
 
-  if (guess < 0) {
-    alert("عدد منفی مجاز نیست!");
-    continue
-  }
 
-  if (guess === magicNumber) {
-    alert("آفرین! درست حدس زدی 🎉");
-    break;
-  } else {
-    alert("اشتباهه! دوباره امتحان کن...");
-  }
 
-  attempt === 5 && alert("تموم شد! عدد جادویی " + magicNumber + " بود.");
-}
+
+
