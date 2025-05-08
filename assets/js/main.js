8f8e5aed2442f114c4b3eaf07d0f14e536d5ba14
baqaB4731 <baqa4731@gmail.com>
2025-05-08 22:54:20 +0330

    after EXP string metod video 55

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,30 @@
-const text1 = "abbas";
-const text2 = "my name is abbas";
+function analyzeText() {
+  const input = document.getElementById("inputText").value;
+  const output = document.getElementById("output");
 
-console.log(text1.repeat(3));
+  if (input == "") {
+    alert("لطفا متن را وارد کنید")
+    return
+  }
 
 
+  let message = "";
 
+  // استفاده از چند متد مهم رشته‌ای:
+  message += `<p>طول متن: ${input.length}</p>`;
+  message += `<p>حروف بزرگ: ${input.toUpperCase()}</p>`;
+  message += `<p>آیا شامل 'js' است؟ ${input.includes("js") ? "بله" : "خیر"}</p>`;
+  message += `<p>اولین موقعیت a: ${input.indexOf("a")}</p>`;
+  message += `<p>برعکس متن: ${input.split("").reverse().join("")}</p>`;
+  message += `<p>متن با حروف جداشده: ${input.split("").join("-")}</p>`;
 
+    // استفاده از if برای هشدار طول زیاد متن:
+    if (input.trim().length > 20) {
+      message += `<p style='color: red;'>متن شما طولانی است</p>`;
+    }
 
+    // مثال ternary برای تشخیص شروع متن:
+  message += `<p>${input.startsWith("سلام") ||input.startsWith("hello")   ? "با سلام شروع شده ✅" : "شروع متفاوت است ❌"}</p>`;
 
+  output.innerHTML = message;
+}
