058b25371eeea10241f1e22a14243ced95431ff3
baqaB4731 <baqa4731@gmail.com>
2025-05-21 23:46:27 +0330

    after setset timeout video 67

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,6 @@
-// <!-- Clipboard Events -->
-document.getElementById("clipInput").addEventListener("cut", () => {
-  alert("کپی شد با JS!");
-});
+setTimeout(() => {
+  console.log("A");
+}, 1000);
 
-// <!-- Drag & Drop -->
-
-document.getElementById("dragBox").addEventListener("dragend", () => {
-  console.log("درگ شروع شد با JS!");
-});
-
-// <!-- Window Events -->
-window.addEventListener("scroll", () => {
-  console.log("سایز پنجره تغییر کرد!");
-});
-
-
-// <!-- Media Events -->
-const video = document.getElementById("videoEl");
-video.addEventListener("seeking", () => console.log("ویدیو با JS پخش شد!"));
-video.addEventListener("volumechange", () => console.log("ویدیو با JS متوقف شد!"));
-
+setTimeout("console.log(333)", 200);
 
