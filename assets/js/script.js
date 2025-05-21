0d6065f4d9d236eaa2632af083e34d4423bf9ae2
baqaB4731 <baqa4731@gmail.com>
2025-05-21 23:08:00 +0330

    other events video 66

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
-// <!-- Mouse Events -->
-document.getElementById("mouseBtn").addEventListener("contextmenu", function () {
-  alert("کلیک راست شد با JS!");
+// <!-- Clipboard Events -->
+document.getElementById("clipInput").addEventListener("cut", () => {
+  alert("کپی شد با JS!");
 });
 
-// <!-- Keyboard Events -->
-document.getElementById("keyboardInput").addEventListener("keyup", () => {
-  console.log("کلیدی فشرده شد با JS!");
+// <!-- Drag & Drop -->
+
+document.getElementById("dragBox").addEventListener("dragend", () => {
+  console.log("درگ شروع شد با JS!");
 });
 
-// <!-- Form Events -->
-
-document.getElementById("checkBox").addEventListener("change", function () {
-  console.log("وضعیت چک باکس:", this.checked);
+// <!-- Window Events -->
+window.addEventListener("scroll", () => {
+  console.log("سایز پنجره تغییر کرد!");
 });
+
+
+// <!-- Media Events -->
+const video = document.getElementById("videoEl");
+video.addEventListener("seeking", () => console.log("ویدیو با JS پخش شد!"));
+video.addEventListener("volumechange", () => console.log("ویدیو با JS متوقف شد!"));
+
+
