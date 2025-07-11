9d2bb567debbd26bdd96f38480ac8a69a84c3024
baqaB4731 <baqa4731@gmail.com>
2025-07-11 22:52:43 +0330

    after fetch topic video 72

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// const obj = new Object();
-// obj.name = "عباس";
-// console.log(obj); // { name: "عباس" }
-
-
-// const arr = new Array(3);
-// console.log(arr); // [empty × 3]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,9 @@
-
-
-
-const pizzaPromise = new Promise((resolve , reject)=>{
-  let isReady = false
-
-  if(isReady){
-     resolve('پیتزای شما آماده شد!');
-  }else{
-     reject('پیتزای شما لغو شد!');
-  }
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(response => {
+  console.log(response);
+  return response.json()
+}).then(data =>{
+  console.log(data);
+  
 })
-
-pizzaPromise .then((message)=>{
-  console.log("موفقیت:", message);
-}).catch((error)=>{
-console.log("خطا:", error);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const pizzaPromise = new Promise((resolve, reject) => {
-//   const isPizzaReady = true;
-
-//   if (isPizzaReady) {
-//     resolve("🍕 پیتزای شما آماده است!");
-//   } else {
-//     reject("❌ سفارش شما لغو شد.");
-//   }
-// });
-
-// // گرفتن نتیجه با then و catch
-// pizzaPromise
-//   .then((message) => {
-//     console.log("موفقیت:", message);
-//   })
-//   .catch((error) => {
-//     console.log("خطا:", error);
-//   });
+.catch(err => console.log("خطا:", err))
