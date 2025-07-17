708039e4c45df7ecf2429ef7c11fceebe6eb7334
baqaB4731 <baqa4731@gmail.com>
2025-07-17 13:30:05 +0330

    after async , await topic video 77

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,28 @@
-const tbody = document.getElementById("tbody");
-function loadUsers() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => {
-
-      document.getElementById("table").style.opacity = "1";
-      tbody.innerHTML = "";
-      data.forEach(user => {
-        tbody.innerHTML += `<tr id="user-${user.id}"> 
-              <td>${user.id}</td>
-              <td>${user.name}</td>
-              <td>${user.phone}</td>
-              <td>${user.email}</td>
-              <td>${user.address.city}, ${user.address.street}</td>
-              <td>
-              <button  onclick="deleteUser(${user.id})">حذف</button>
-              <button onclick="editUser(${user.id}, '${user.name}', '${user.email}')">ویرایش</button>
-              </td>
-            </tr>`;
-      });
-    });
+function getCoffee() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("☕ قهوه آماده شد");
+      resolve("قهوه");
+    }, 2000);
+  });
 }
 
-// اضافه کردن کاربر 
+// console.log("🔹 صبحانه می‌خورم");
+// getCoffee().then(coffee => {
+//   console.log("🔹 حالا که " + coffee + " گرفتم، میرم سر کار");
+// });
+// console.log("🔹 رفتم سر کار");
 
-function addUser() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
 
-  fetch('https://jsonplaceholder.typicode.com/users', {
-    method: 'POST',
-    body: JSON.stringify({ name: name, email: email }),
-    headers: { 'Content-type': 'application/json; charset=UTF-8' }
-  })
-    .then(res => res.json())
-    .then(user => {
-      alert("کاربر با موفقیت ارسال شد (ولی ذخیره نمی‌شود چون API تستی است)");
-      console.log("POST response:", user);
 
-      tbody.innerHTML += `<tr>
-        <td>${user.id}</td>
-        <td>${user.name}</td>
-        <td>  </td>
-        <td>${user.email}</td>
-        <td>   </td>
-        <td><button>حذف</button></td>
-      </tr>`;
-
-    });
+async function startDay() {
+  console.log("🔹 صبحانه می‌خورم");
+  const coffee = await getCoffee();
+  console.log("🔹 حالا که " + coffee + " گرفتم، میرم سر کار");
+  console.log("🔹 رفتم سر کار");
 }
 
-
-// حذف کاربر
-
-function deleteUser(id) {
-  fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
-    method: 'DELETE'
-  })
-    .then(() => {
-      alert(`کاربر با شناسه ${id} حذف شد (فقط در ظاهر، چون API واقعی نیست)`);
-      loadUsers(); // بازخوانی لیست
-    });
-}
-
-
-// ویرایش کاربر 
-function editUser(id, oldName, oldEmail) {
-  const newName = prompt("نام جدید را وارد کنید:", oldName);
-  const newEmail = prompt("ایمیل جدید را وارد کنید:", oldEmail);
-
-  if (!newName || !newEmail) {
-    alert("ورودی نامعتبر بود");
-    return;
-  }
-
-  fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
-    method: 'PATCH',
-    body: JSON.stringify({ name: newName, email: newEmail }),//name , email
-    headers: { 'Content-type': 'application/json; charset=UTF-8' }
-  })
-    .then(res => res.json())
-    .then(updated => {
-      console.log(updated);
-
-      alert("ویرایش انجام شد (نمایشی)");
-
-
-
-      // اعمال تغییرات به صورت نمایشی
-      const row = document.getElementById(`user-${id}`);
-      row.children[1].textContent = updated.name;
-      row.children[3].textContent = updated.email;
-
-    });
-
-}
+startDay()
 
 
 
@@ -124,9 +52,107 @@ function editUser(id, oldName, oldEmail) {
 
 
 
-//
 
-//
+
+// const tbody = document.getElementById("tbody");
+// function loadUsers() {
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(data => {
+
+//       document.getElementById("table").style.opacity = "1";
+//       tbody.innerHTML = "";
+//       data.forEach(user => {
+//         tbody.innerHTML += `<tr id="user-${user.id}">
+//               <td>${user.id}</td>
+//               <td>${user.name}</td>
+//               <td>${user.phone}</td>
+//               <td>${user.email}</td>
+//               <td>${user.address.city}, ${user.address.street}</td>
+//               <td>
+//               <button  onclick="deleteUser(${user.id})">حذف</button>
+//               <button onclick="editUser(${user.id}, '${user.name}', '${user.email}')">ویرایش</button>
+//               </td>
+//             </tr>`;
+//       });
+//     });
+// }
+
+// // اضافه کردن کاربر
+
+// function addUser() {
+//   const name = document.getElementById("name").value;
+//   const email = document.getElementById("email").value;
+
+//   fetch('https://jsonplaceholder.typicode.com/users', {
+//     method: 'POST',
+//     body: JSON.stringify({ name: name, email: email }),
+//     headers: { 'Content-type': 'application/json; charset=UTF-8' }
+//   })
+//     .then(res => res.json())
+//     .then(user => {
+//       alert("کاربر با موفقیت ارسال شد (ولی ذخیره نمی‌شود چون API تستی است)");
+//       console.log("POST response:", user);
+
+//       tbody.innerHTML += `<tr>
+//         <td>${user.id}</td>
+//         <td>${user.name}</td>
+//         <td>  </td>
+//         <td>${user.email}</td>
+//         <td>   </td>
+//         <td><button>حذف</button></td>
+//       </tr>`;
+
+//     });
+// }
+
+
+// // حذف کاربر
+
+// function deleteUser(id) {
+//   fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+//     method: 'DELETE'
+//   })
+//     .then(() => {
+//       alert(`کاربر با شناسه ${id} حذف شد (فقط در ظاهر، چون API واقعی نیست)`);
+//       loadUsers(); // بازخوانی لیست
+//     });
+// }
+
+
+// // ویرایش کاربر
+// function editUser(id, oldName, oldEmail) {
+//   const newName = prompt("نام جدید را وارد کنید:", oldName);
+//   const newEmail = prompt("ایمیل جدید را وارد کنید:", oldEmail);
+
+//   if (!newName || !newEmail) {
+//     alert("ورودی نامعتبر بود");
+//     return;
+//   }
+
+//   fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+//     method: 'PATCH',
+//     body: JSON.stringify({ name: newName, email: newEmail }),//name , email
+//     headers: { 'Content-type': 'application/json; charset=UTF-8' }
+//   })
+//     .then(res => res.json())
+//     .then(updated => {
+//       console.log(updated);
+
+//       alert("ویرایش انجام شد (نمایشی)");
+
+
+
+//       // اعمال تغییرات به صورت نمایشی
+//       const row = document.getElementById(`user-${id}`);
+//       row.children[1].textContent = updated.name;
+//       row.children[3].textContent = updated.email;
+
+//     });
+
+// }
+
+
 
 
 
